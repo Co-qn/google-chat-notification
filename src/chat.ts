1b0fda4868059b19c855b159df5e44acf48242f1
Andrea Avanzi <andrea.avanzi@ctinnovation.it>
2021-08-02 23:37:34 +0200

    fix

--- src/chat.ts
+++ src/chat.ts
@@ -45,9 +45,10 @@ export async function notify(name: string, url: string, status: Status, artifact
           widgets: [
             {
               "textParagraph": {
-                "text": `<b>Commit ID:</b> ${sha.substring(0, 8)}<br>
-                        <b>Status:</b> <font color="${statusColorPalette[status]}">${statusText[status]}</font><br>
-                        <b>Event:</b> ${eventName}<br><b>Artifact:</b> <a href="${artifactUrl}">${artifactUrl}</a>`
+                "text": `<b>Commit ID:</b> ${sha.substring(0, 8)}
+                <b>Status:</b> <font color="${statusColorPalette[status]}">${statusText[status]}</font>
+                <b>Event:</b> ${eventName}
+                <b>Artifact:</b> <a href="${artifactUrl}">${artifactUrl}</a>`
               }
             },
             {
