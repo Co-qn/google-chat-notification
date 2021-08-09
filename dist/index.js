5cb4be158c733cfbe0a6fd0a4bc4f24d732076f6
Andrea Avanzi <andrea.avanzi@ctinnovation.it>
2021-08-09 14:52:32 +0200

    fix

--- dist/index.js
+++ dist/index.js
@@ -2496,7 +2496,7 @@ function notify(name, url, status, artifactUrl) {
                             widgets: [
                                 {
                                     "textParagraph": {
-                                        "text": `<b>Commit ID:</b> ${sha.substring(0, 8)}<br><b>Status:</b> <font color="${statusColorPalette[status]}">${statusText[status]}</font><br><b>Event:</b> ${eventName}<br><b>Artifact:</b> ${artifactUrl}`
+                                        "text": `<b>Commit ID:</b> ${sha.substring(0, 8)}<br><b>Status:</b> <font color="${statusColorPalette[status]}">${statusText[status]}</font><br><b>Event:</b> ${eventName}<br><b>Artifact:</b> <a href=${artifactUrl}>${artifactUrl}</a>`
                                     }
                                 },
                                 // {
